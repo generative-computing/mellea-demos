705e87a21ed25d3811c7999ab76566abfeb0f764
Paul S. Schweigert <paul@paulschweigert.com>
2026-03-18 14:39:02 -0400

    drop container builds
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- 2026-atai/containers/langflow-intrinsics/components/Intrinsics/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Langflow Intrinsic Components
-
-This module contains specialized intrinsic components and retrieval components
-for the conversational AI intrinsics server.
-"""
-
-__all__ = []
